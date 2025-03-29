0c73459e2a123374995799f7ac83c2f83fa3ec75
ilhanbenli <ilhanbenli5@gmail.com>
2025-03-29 20:07:43 +0000

    update 2

---
+++ wp-content/plugins/superb-blocks/blocks/recent-posts/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c198d4eeff65c4880a17');
